162d335d617e80ea47d13ee7a57636671e951295
MrBigoudi <yannis.kedadry2@gmail.com>
2025-03-21 17:50:40 +0100

    reduction rule 5

--- set_cover_measure_conquer_v2.py
+++ set_cover_measure_conquer_v2.py
@@ -56,7 +56,7 @@ def minimum_set_cover(S, U):
     return smallest_set_cover
 
 
-def minimum_set_cover_reduction_rule1(S, U):
+def minimum_set_cover_reduction_rules(S, U):
     """
     Recursively computes the minimum set cover.
     In this version of the MSC, if there is an element of frequency 1 with the size
@@ -69,103 +69,35 @@ def minimum_set_cover_reduction_rule1(S, U):
     if len(S) == 0:
         return set() if len(U) == 0 else None
     
-    #check if there an element of frequency 1
+    # Reduction Rule 1
     counter = dict()
-    s_min = None
     for s in S:
         for e in s:
-            counter[e] = counter.get(e, (0, False)) + (1, False) 
-        if len(s) == 1:
-            counter[e] = (counter[e][0], True)
-    frequence_1_exist = False
-    new_U = U
-    S_removed = S
+            counter[e] = counter.get(e, (0, s))
+            counter[e] = (counter[e][0] + 1, s)
     for e in counter:
-        if counter[e][0] == 1 and counter[e][1]:
-            frequence_1_exist = True
-            R = frozenset({e})
-            S_removed -= {R}
-            new_U -=  R
-            S_removed = {s - R for s in S_removed if len(s - R) > 0}
-    if frequence_1_exist:
-        cover = minimum_set_cover_reduction_rule1(S_removed, new_U)
-        cover.add(R)
-        return cover
+        if counter[e][0] == 1:
+            R = counter[e][1]
+            new_U = U - R
+            S_rest = S - {R}
+            S_removed = {s - R for s in S_rest if len(s - R) > 0}
+            # print(f"\n\ne: {e}, R: {R}\nS: {S}\ncounter: {counter}\nS_rest: {S_rest}\nS_removed: {S_removed}\nnew_U: {new_U}\n\n")
 
-    # Select the set with maximum cardinality (largest subset)
-    S_max = max(S, key=len)
-    S_rest = S - {S_max}  # Remove selected subset from S
-    S_removed = {s - S_max for s in S_rest if len(s - S_max) > 0} #Each element that are in S_max is removed from S
-    
-    #we need a mappint from s_removed to S_rest to get the original mapping
-    s_map = dict()
-    for s in S_rest:
-        removed = s - S_max
-        s_map[removed] = s
+            s_map = dict()
+            for s in S_rest:
+                removed = s - R
+                if len(removed) > 0:
+                    s_map[removed] = s
 
-    # Case 1: Take S_max in the set cover
-    new_U = U - S_max  # Remove covered elements from U
-    cover_with_S_max = minimum_set_cover_reduction_rule1(S_removed, new_U)
+            cover = minimum_set_cover_reduction_rules(S_removed, new_U)
 
-    if cover_with_S_max is not None:
-        #get the original mapping from the minimum set
-        cover_with_S_max = {frozenset(s_map[s]) for s in cover_with_S_max}
-        cover_with_S_max.add(S_max)
-        #print(cover_with_S_max)
-    
-    # Case 2: Do not take S_max in the set cover
-    cover_without_S_max = minimum_set_cover_reduction_rule1(S_rest, U)
-    
-    # Return None if both cover sets are None
-    if cover_without_S_max is None and cover_with_S_max is None:
-        return None
-
-    # Take the smallest between cover_with_S_max and cover_without_S_max
-    if cover_with_S_max is None:
-        smallest_set_cover = cover_without_S_max
-    elif cover_without_S_max is None:
-        smallest_set_cover = cover_with_S_max
-    else:
-        smallest_set_cover = cover_with_S_max if len(cover_with_S_max) < len(cover_without_S_max) else cover_without_S_max
-    
-    return smallest_set_cover
-
-
-def minimum_set_cover_reduction_rule3(S, U):
-    """
-    Recursively computes the minimum set cover.
-    In this version of the MSC, if there is an element of frequency 1 with the size
-    of 1 and frequency of 1
-    :param S: A set of subsets (set of frozensets).
-    :param U: The universal set that needs to be covered (set).
-    :return: The minimum set cover as a set of frozensets or None if no cover exists.
-    """
-    # Base case: If no more subsets are left
-    if len(S) == 0:
-        return set() if len(U) == 0 else None
-    
-    #check if there an element of frequency 1
-    counter = dict()
-    s_min = None
-    for s in S:
-        for e in s:
-            counter[e] = counter.get(e, (0, False)) + (1, False) 
-        if len(s) == 1:
-            counter[e] = (counter[e][0], True)
-    frequence_1_exist = False
-    new_U = U
-    S_removed = S
-    for e in counter:
-        if counter[e][0] == 1 and counter[e][1]:
-            frequence_1_exist = True
-            R = frozenset({e})
-            S_removed -= {R}
-            new_U -=  R
-            S_removed = {s - R for s in S_removed if len(s - R) > 0}
-    if frequence_1_exist:
-        cover = minimum_set_cover_reduction_rule3(S_removed, new_U)
-        cover.add(R)
-        return cover
+            if cover is not None:
+                #get the original mapping from the minimum set
+                cover = {frozenset(s_map[s]) for s in cover}
+                cover.add(R)
+                return cover
+            else:
+                return {R}
 
     # Reduction Rule 3
     for Q in S:
@@ -177,7 +109,41 @@ def minimum_set_cover_reduction_rule3(S, U):
                         is_R_a_Q_subset = False
                         break
                 if is_R_a_Q_subset:
-                    return minimum_set_cover_reduction_rule3(S - {R}, U)
+                    # print(f"\n\nQ: {Q}, R: {R}\n\nS: {S}\n\nS - R: {S-{R}}\n\n")
+                    return minimum_set_cover_reduction_rules(S - {R}, U)
+
+    # Reduction Rule 5
+    elements_to_sets = dict()
+    for s in S:
+        for e in s:
+            if e not in elements_to_sets:
+                elements_to_sets[e] = set()
+            elements_to_sets[e].add(s)
+    sorted_elements_to_sets = sorted(elements_to_sets.keys(), key=lambda e: len(elements_to_sets[e]))
+    removed_elements = set()
+    for i, e1 in enumerate(sorted_elements_to_sets):
+        if e1 in removed_elements:
+            continue
+        for e2 in sorted_elements_to_sets[i+1:]:
+            if e2 in removed_elements:
+                continue
+            if elements_to_sets[e1] <= elements_to_sets[e2]: # elements_to_sets[e1] include in elements_to_sets[e2]
+                removed_elements.add(e2)
+    if len(removed_elements) > 0:
+        new_U = U - removed_elements
+        s_map = dict()
+        for s in S:
+            removed = s - removed_elements
+            if len(removed) > 0:
+                s_map[removed] = s
+        S_removed = {s - removed_elements for s in S if len(s - removed_elements) > 0}
+        cover = minimum_set_cover_reduction_rules(S_removed, new_U)
+        if cover is not None:
+            #get the original mapping from the minimum set
+            cover = {frozenset(s_map[s]) for s in cover}
+            return cover
+        else:
+            return None
 
     # Select the set with maximum cardinality (largest subset)
     S_max = max(S, key=len)
@@ -188,20 +154,22 @@ def minimum_set_cover_reduction_rule3(S, U):
     s_map = dict()
     for s in S_rest:
         removed = s - S_max
-        s_map[removed] = s
+        if len(removed) > 0:
+            s_map[removed] = s
 
     # Case 1: Take S_max in the set cover
     new_U = U - S_max  # Remove covered elements from U
-    cover_with_S_max = minimum_set_cover_reduction_rule3(S_removed, new_U)
+    cover_with_S_max = minimum_set_cover_reduction_rules(S_removed, new_U)
 
     if cover_with_S_max is not None:
         #get the original mapping from the minimum set
         cover_with_S_max = {frozenset(s_map[s]) for s in cover_with_S_max}
         cover_with_S_max.add(S_max)
-        #print(cover_with_S_max)
+    else:
+        cover_with_S_max = {S_max}
     
     # Case 2: Do not take S_max in the set cover
-    cover_without_S_max = minimum_set_cover_reduction_rule3(S_rest, U)
+    cover_without_S_max = minimum_set_cover_reduction_rules(S_rest, U)
     
     # Return None if both cover sets are None
     if cover_without_S_max is None and cover_with_S_max is None:
@@ -306,7 +274,8 @@ def test():
     #print(f"U: {U}")
     print(f"S: {S}")
 
-    solution = minimum_set_cover_reduction_rule3(S, U)
+    # solution = minimum_set_cover(S, U)
+    solution = minimum_set_cover_reduction_rules(S, U)
     print(f"length of solution {len(solution)}")
     print("Minimum Set Cover:", solution)
 
@@ -320,14 +289,15 @@ def test():
 
 if __name__ == "__main__":
     #a = frozenset({5})
-    test()
+    # test()
     # """
     #graph_folder_path = "generated_graphs_increasing_edge"
     
-    # graph_folder_path = "generated_graphs_increasing_vertices"
-    # execution_times = test_complexity(minimum_set_cover_reduction_rule3, graph_folder_path)
-    # plot_execution_time(execution_times, save_name="execution_time_vertices.png", lower_bound=5, upper_bound=60)
-    # print(execution_times)
+    graph_folder_path = "generated_graphs_increasing_vertices"
+    # execution_times = test_complexity(minimum_set_cover, graph_folder_path)
+    execution_times = test_complexity(minimum_set_cover_reduction_rules, graph_folder_path)
+    plot_execution_time(execution_times, save_name="execution_time_vertices.png", lower_bound=5, upper_bound=60)
+    print(execution_times)
 
     # """
     """file_path = "./generated_graphs_increasing_vertices/graph_n55_p_0.5.gr"
